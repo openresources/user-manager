e272f722fedc45a94edeb66b42624bfd4ae5f75e
Eviano Afiemo <6235534+evianoa@users.noreply.github.com>
2020-06-20 03:08:49 +0100

    style: add code to fix x-cloak issue

--- resources/views/layouts/app_shell.blade.php
+++ resources/views/layouts/app_shell.blade.php
@@ -13,39 +13,20 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @stack('vendor-assets')
     @stack('styles')
+
+    <style>
+        /* always hidden */
+        [x-cloak=""] { display: none; }
+
+        /* hidden on mobile/smaller screens */
+        @media screen and (max-width: 768px) {
+            [x-cloak="mobile"] { display: none; }
+        }
+    </style>
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none">
     <div id="app">
-        <nav class="bg-blue-900 shadow py-6">
-            <div class="container mx-auto px-6 md:px-0">
-                <div class="flex items-center justify-center">
-                    <div class="mr-6">
-                        <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
-                            {{ config('app.name', 'Laravel') }}
-                        </a>
-                    </div>
-                    <div class="flex-1 text-right">
-                        @guest
-                            <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            @if (Route::has('register'))
-                                <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            @endif
-                        @else
-                            <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
-
-                            <a href="{{ route('logout') }}"
-                               class="no-underline hover:underline text-gray-300 text-sm p-3"
-                               onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                                {{ csrf_field() }}
-                            </form>
-                        @endguest
-                    </div>
-                </div>
-            </div>
-        </nav>
-
+        @include('user-manager::partials.menus.site_nav')
         @yield('scaffold')
     </div>
 
