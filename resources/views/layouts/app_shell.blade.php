5a2b99ddfe0464caf87d5fcd5a022c64f42b0e8b
Eviano Afiemo <6235534+evianoa@users.noreply.github.com>
2020-07-28 02:46:04 +0100

    refactor: make menu easiser to extend

--- resources/views/layouts/app_shell.blade.php
+++ resources/views/layouts/app_shell.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,18 +16,28 @@
 
     <style>
         /* always hidden */
-        [x-cloak=""] { display: none; }
+        [x-cloak=""] {
+            display: none;
+        }
 
         /* hidden on mobile/smaller screens */
         @media screen and (max-width: 768px) {
-            [x-cloak="mobile"] { display: none; }
+            [x-cloak="mobile"] {
+                display: none;
+            }
         }
     </style>
 </head>
+
 <body class="bg-gray-100 h-screen antialiased leading-none">
     <div id="app">
         <header>
-            @stack('site-nav')
+            @yield('navbar-main')
+            @sectionMissing('navbar-main')
+                <nav class="bg-blue-900 shadow py-3" aria-labelledby="site-navigation">
+                    @include('user-manager::partials.menus.navbar_main')
+                </nav>
+            @endif
         </header>
         <main>
             <div>
@@ -45,4 +56,5 @@
     <!-- Scripts -->
     @stack('scripts')
 </body>
+
 </html>
--- resources/views/layouts/scaffold.blade.php
+++ resources/views/layouts/scaffold.blade.php
@@ -1,9 +1,5 @@
 @extends(config('user-manager.app_shell_template'))
 
-@push('site-nav')
-    @include('user-manager::partials.menus.site_nav')
-@endpush
-
 @push('vendor-assets')
 <link href="{{ asset('vendor/user-manager/css/app.css') }}" rel="stylesheet">
 <script src="{{ asset('vendor/user-manager/js/app.js') }}" defer></script>
